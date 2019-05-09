f266b32b6032cf3cf16cfc5b6b7881534446710c
Sherin Samuel <32221152+sherinsamuel@users.noreply.github.com>
2019-05-09 12:13:56 +0530

    Update MPU6050_DMP6.ino

--- MPU6050_DMP6.ino
+++ MPU6050_DMP6.ino
@@ -71,11 +71,10 @@ void setup() {
     #endif
 
     // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
+    // baud rate really up to you depending on your project)
     Serial.begin(57600);
     mySerial.begin(57600); // For Bluetooth
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    while (!Serial); // wait for enumeration
 
     // initialize device
     mySerial.println(F("Initializing I2C devices..."));
